4b91d8fa080e9e8ccdf8d62b7c601861b5f25edb
instaclustr-wenbodu <wenbo.du@netapp.com>
2023-12-15 11:24:05 +1100

    update

--- openapi/common.go
+++ openapi/common.go
@@ -532,6 +532,7 @@ func convertObjectToLocalStateData(property *SpecSchemaDefinitionProperty, prope
 		// Here we are processing the items of the list which are objects. In this case we need to keep the original
 		// types as Terraform honors property types for resource schemas attached to TypeList properties
 		propValue, err := convertPayloadToLocalStateDataValue(schemaDefinitionProperty, propertyValue, localStateMapValue[propertyName], false)
+		log.Printf("[INFO] propValue: %s", propValue)
 		if err != nil {
 			return nil, err
 		}
